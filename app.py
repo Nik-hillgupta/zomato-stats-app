45b08ea7ccd840cd55589bc08d1a28d4dfd645e1
Nik-hillgupta <nikhilgup99@gmail.com>
2025-07-10 14:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown("Get insights on your Zomato spending directly from your Gmail.")
 # OAuth Setup
 client_id = st.secrets["gmail"]["client_id"]
 client_secret = st.secrets["gmail"]["client_secret"]
-redirect_uri = "https://zomato-stats-app-plvlspp2pgvbokc5hokpfb.streamlit.app/"
+redirect_uri = "https://zomato-stats-app-plvlspp2pgvbokc5hokpfb.streamlit.app"
 auth_url = "https://accounts.google.com/o/oauth2/auth"
 token_url = "https://oauth2.googleapis.com/token"
 scope = "https://www.googleapis.com/auth/gmail.readonly"

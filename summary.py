d1c959e7a6c3e6894e39210fa3cccc0cbd11c298
Nik-hillgupta <nikhilgup99@gmail.com>
2025-07-10 18:08:00 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 def generate_summary(df):
     df["order_date"] = pd.to_datetime(df["order_date"], errors="coerce")
+
     total_orders = len(df)
     total_amount = df["amount"].sum()
 
